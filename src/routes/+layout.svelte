76f0fe36b3b58b2e02b0f812b6f9ce4e5350a219
Alejandro Brozzo <alejandro.brozzo@wunderdog.fi>
2023-09-30 17:32:55 +0200

    Some pages and layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,33 @@
+<nav />
+<aside>
+  <div class="nav">
+    <h2><a href="/">Home</a></h2>
+    <h2><a href="/dice">Dice</a></h2>
+    <h2><a href="/cards">Cards</a></h2>
+    <h2><a href="/numbers">Numbers</a></h2>
+  </div>
+</aside>
+<main>
+  <slot />
+</main>
+
+<style>
+  .nav {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    background-color: gray;
+  }
+
+  h2 {
+    width: 50%;
+    background-color: black;
+    text-align: center;
+  }
+
+  a {
+    color: yellow;
+    text-decoration: none;
+  }
+</style>
