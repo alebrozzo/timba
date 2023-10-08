0e8bcc7a57e38e9a82363774a9b3479d494184c7
Alejandro Brozzo <alejandro.brozzo@wunderdog.fi>
2023-10-08 16:29:43 +0200

    more tests

--- src/lib/tests/diceUtils.test.ts
+++ src/lib/tests/diceUtils.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect, beforeEach } from "vitest"
-import { addDieType, deleteDieType, getDefaultDie } from "$lib/diceUtils"
+import { addDieType, deleteDieType, getDefaultDie, getDiceSetBySlug } from "$lib/diceUtils"
 import type { DiceSet } from "$lib/types"
 import { getDiceCollection } from "../../testUtils"
 
@@ -37,5 +37,25 @@ describe("deleteDieType", () => {
     expect(newSet).not.toBe(set)
     expect(newSet.dice.length).toBe(set.dice.length - 1)
     expect(newSet.dice.find((x) => x.name === dieTypeName)).toBeUndefined()
+  })
+})
+
+describe("getDiceSetBySlug", () => {
+  it("returns a set when it exists", () => {
+    const set = diceSetCollection[0]
+    const newSet = getDiceSetBySlug(diceSetCollection, set.slug)
+    expect(newSet).toBe(set)
+  })
+
+  it("returns null when it does not exist", () => {
+    const newSet = getDiceSetBySlug(diceSetCollection, "not-a-valid-slug")
+    expect(newSet).toBeNull()
+  })
+
+  it("returns the first set when it exists twice", () => {
+    const set = diceSetCollection[0]
+    diceSetCollection.push({ ...set, id: "456" })
+    const newSet = getDiceSetBySlug(diceSetCollection, set.slug)
+    expect(newSet).toBe(set)
   })
 })
