ec288b8e270a29d775c6db6ff90be46eb5747bff
Alejandro Brozzo <alejandro.brozzo@wunderdog.fi>
2023-10-11 14:15:13 +0200

    ui: tabs for main selection, new colors

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,50 @@
-<script>
+<script lang="ts">
   import "../global.css"
+  import Tab, { Icon, Label } from "@smui/tab"
+  import TabBar from "@smui/tab-bar"
+  import { goto } from "$app/navigation"
+
+  type TabEntry = {
+    k: number
+    icon: string
+    label: string
+    url: string
+  }
+  const key = (tab: TabEntry) => tab.k
+
+  let tabs: TabEntry[] = [
+    {
+      k: 1,
+      icon: "🎲",
+      label: "Dice",
+      url: "/dice",
+    },
+    {
+      k: 2,
+      icon: "♠",
+      label: "Cards",
+      url: "/cards",
+    },
+    {
+      k: 3,
+      icon: "🔢",
+      label: "Numbers",
+      url: "/numbers",
+    },
+  ]
+  let active = tabs[0]
+
+  function handleClick(url: string) {
+    goto(url)
+  }
 </script>
+
+<TabBar {tabs} let:tab {key} bind:active>
+  <Tab {tab} stacked indicatorSpanOnlyContent tabIndicator$transition="fade" on:click={() => handleClick(tab.url)}>
+    <Icon class="material-icons">{tab.icon}</Icon>
+    <Label>{tab.label}</Label>
+  </Tab>
+</TabBar>
 
 <main>
   <slot />
