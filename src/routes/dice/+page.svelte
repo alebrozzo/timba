da42ac1b64e38a4047b69708112d28a8add6570b
Alejandro Brozzo <alejandro.brozzo@wunderdog.fi>
2023-10-12 01:05:16 +0200

    ui: buttons

--- src/routes/dice/+page.svelte
+++ src/routes/dice/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import Button, { Label } from "@smui/button"
   import { onMount } from "svelte"
   import { diceSetStore } from "$lib/stores/diceStore"
   import type { PageData } from "./$types"
@@ -9,11 +10,14 @@
   })
 </script>
 
-<div class="button-container">
-  <a class="button" href="/">Home</a>
-  <a class="button" href="dice/new">New</a>
+<div class="display-vertical button-container gallery">
+  <Button href="dice/new" variant="raised">
+    <Label>New</Label>
+  </Button>
 
   {#each data.diceSets as set}
-    <a class="button" href="dice/{set.slug || set.id}">{set.name}</a>
+    <Button href="dice/{set.slug || set.id}" variant="outlined">
+      <Label>{set.name}</Label>
+    </Button>
   {/each}
 </div>
