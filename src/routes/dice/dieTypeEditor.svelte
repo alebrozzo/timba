639c87b60fe4233eee8cf053794193055fd4f118
Alejandro Brozzo <alejandro.brozzo@wunderdog.fi>
2023-10-12 17:46:52 +0200

    ui: more smui buttons

--- src/routes/dice/dieTypeEditor.svelte
+++ src/routes/dice/dieTypeEditor.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import Button, { Label } from "@smui/button"
   import Textfield from "@smui/textfield"
   import HelperText from "@smui/textfield/helper-text"
   import { createEventDispatcher } from "svelte"
@@ -14,15 +15,28 @@
 </script>
 
 <Textfield label="Faces" type="number" required bind:value={dieType.faces}>
-  <HelperText>How many faces per die?</HelperText>
+  <HelperText persistent slot="helper">How many faces per die?</HelperText>
 </Textfield>
 <Textfield label="Count" type="number" required bind:value={dieType.count}>
-  <HelperText>How many dice of this type?</HelperText>
+  <HelperText persistent slot="helper">How many dice of this type?</HelperText>
 </Textfield>
 <Textfield label="Name" input$maxlength={15} bind:value={dieType.name}>
-  <HelperText>Optionally add a name for this die type</HelperText>
+  <HelperText persistent slot="helper">Optionally add a name for this die type</HelperText>
 </Textfield>
 
-<div class="display-vertical editor-container">
-  <button type="button" on:click={() => saveDieType(dieType)}>Save die type</button>
+<div class="minor-button-box" style="margin-top: 1rem;">
+  <Button variant="raised" on:click={() => saveDieType(dieType)}>
+    <Label class="timba-small-button">Save die type</Label>
+  </Button>
+  <Button variant="raised" on:click={() => saveDieType(dieType)}>
+    <Label class="timba-small-button">Cancel</Label>
+  </Button>
 </div>
+
+<style>
+  .minor-button-box {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+  }
+</style>
